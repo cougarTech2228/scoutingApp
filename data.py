78576e7e2d0261ff4bc6cd61125f605a2cced04c
Erich Spaker <erichspaker@gmail.com>
2014-01-02 16:38:15 -0500

    worked on data modual

--- data.py
+++ data.py
@@ -4,23 +4,41 @@ import main
 import user
 import joy
 
+class competition:
+    def __init__(numMatches, name):
+        #if numMatches = 0 then number of matches in competician is unknown
+        #name is a name given for this competitian ex) 'FLR seeding', 'FLR final', 'nationals Finals'
+        matchList[]
+        matchlist.append(0)
+    def newMatch(robots):
+        matchList[0] += 1
+        matchlist.append(match(self, matchList[0], robots))
+    
+        
+            
+        
+
 # an instance of a match with a number and six robot objects
 class match:
-	def __init__(num, robotNums):
+	def __init__(comp, num, robotNums):
 		matchNum = num
-		robots = []
+		robots = [] # 0-2 red, 3-5 blue
 		n = 0
 		for r in robotNums:
-			robots[n] = robot(r,n)
+			robots[n] = robot(self, r,n)
 			n += 1
 	
 	
-# an  instance of a robot with team number and alliance (will later have variables for collecting match data and possibly a variable for its joystick)
-class robot:
-	def __init__(team,roboNumber):
+# an  instance of a robot for a specific match in a specific competitian with team number and alliance (will later have variables for collecting match data and possibly a variable for its joystick)
+class robot: 
+	def __init__(myMatch, team,roboNumber):
 		if roboNumber <3:
-			alliance = 0 
+			alliance = RED
 		else:
-			alliance = 0
+			alliance = BLUE
+        records = roboRecords(match.comp.name,myMatch.matchNum,alliance)
 			
-	
+class roboRecords:
+    def __init__(comp, myMatch, robot, alliance)
+        # variable being recorded
+                
--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import user
 import data
 import joy
 
+RED = 1
+BLUE = 2
+
+
 pygame.init()
 pygame.event.set_allowed(10)
 joy.joystick_init()
