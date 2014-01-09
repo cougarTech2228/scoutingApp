f72cacdadc11a6408e130497594178b23f11e860
Joseph Moore <josephmoore1994@gmail.com>
2014-01-08 21:17:34 -0500

    Cleaned up after merge, changed some formatting to align with PEP8, fixed some syntax issues

--- data.py
+++ data.py
@@ -1,44 +1,48 @@
-#database module
+# Database module
 
-import main
+#import main
 import user
 import joy
 
-class competition:
-    def __init__(numMatches, name):
-        #if numMatches = 0 then number of matches in competician is unknown
-        #name is a name given for this competitian ex) 'FLR seeding', 'FLR final', 'nationals Finals'
-        matchList[]
-        matchlist.append(0)
-    def newMatch(robots):
-        matchList[0] += 1
-        matchlist.append(match(self, matchList[0], robots))
+class Competition:
     
+    def __init__(self, numMatches, name):
+        # If numMatches = 0 then number of matches in competician is unknown
+        # name is a name given for this competitian ex) 'FLR seeding', 'FLR
+        # final', 'nationals Finals'
+        self.matchList = []
+        self.matchlist.append(0)
         
-            
-        
+    def newMatch(self, robots):
+        self.matchList[0] += 1
+        self.matchlist.append(match(self, matchList[0], robots))        
+
 
 # an instance of a match with a number and six robot objects
-class match:
-	def __init__(comp, num, robotNums):
-		matchNum = num
-		robots = [] # 0-2 red, 3-5 blue
-		n = 0
-		for r in robotNums:
-			robots[n] = robot(self, r,n)
-			n += 1
+class Match:
+    
+    def __init__(self, comp, num, robotNums):
+        matchNum = num
+        robots = [] # 0-2 red, 3-5 blue
+        n = 0
+        for r in robotNums:
+            robots[n] = robot(self, r, n)
+            n += 1
 	
 	
 # an  instance of a robot for a specific match in a specific competitian with team number and alliance (will later have variables for collecting match data and possibly a variable for its joystick)
-class robot: 
-	def __init__(myMatch, team,roboNumber):
-		if roboNumber <3:
-			alliance = RED
-		else:
-			alliance = BLUE
+class Robot:
+    
+    def __init__(self, myMatch, team,roboNumber):
+        if roboNumber < 3:
+            alliance = RED
+        else:
+            alliance = BLUE
         records = roboRecords(match.comp.name,myMatch.matchNum,alliance)
+
 			
-class roboRecords:
-    def __init__(comp, myMatch, robot, alliance)
+class RoboRecords:
+    def __init__(self, comp, myMatch, robot, alliance):
+        pass
         # variables being recorded ex)shots missed, points scored, climberlevel reached
                 
--- main.py
+++ main.py
@@ -12,9 +12,5 @@ BLUE = 2
 
 pygame.init()
 pygame.event.set_allowed(10)
-<<<<<<< HEAD
-joy.joystick_init()
 terminalInterface = user.CLI()
-=======
 joy.joystick_init(True)
->>>>>>> master
