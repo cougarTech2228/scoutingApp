48fe313932e1346e8ec7a535e0f0678b64c61276
Cougar Tech <cougarTech2228@users.noreply.github.com>
2014-01-10 19:13:04 -0500

    finished fixing data structure

--- data.py
+++ data.py
@@ -3,22 +3,43 @@
 ##import main
 ##import user
 ##import joy
+'''
+ 
+
++class competition:
+
++    def __init__(numMatches, name):
+
++        #if numMatches = 0 then number of matches in competician is unknown
+
++        #name is a name given for this competitian ex) 'FLR seeding', 'FLR final', 'nationals Finals'
+
++        matchList[]
+
++        matchlist.append(0)
+
++    def newMatch(robots):
+
++        matchList[0] += 1
+
++        matchlist.append(match(self, matchList[0], robots))
+
+'''
 
 class Competition(list):
     
-    def __init__(self, numMatches=0, name="Test"):
+    def __init__(self, numberOfMatches=0, name="Test"):
         # If numMatches = 0 then number of matches in competician is unknown
         # name is a name given for this competitian ex) 'FLR seeding', 'FLR
         # final', 'nationals Finals'
         self.name = name
-        self.current_match = 0
-        self.last_match = 0
+        self.numMatches = numberOfMatches
         
     def newMatch(self, robots):
         # New Match takes a list of robot numbers as an argument
-        ##self[0] += 1
-        self.append(Match()
-
+        self.inMatches=len(self)+1
+        self.append(Match(self.inMatches, robots, self.name))
+        
     def editMatch(self, matchNumber, teamNumbers):
         self[matchNumber-1] = Match(self.last_match, teamNumbers)
 
@@ -28,14 +49,15 @@ class Competition(list):
 # an instance of a match with a number and six robot objects
 class Match:
     
-    def __init__(self, num, teamNumbers, comp):
+    def __init__(self, num, teamNumbers, compName):
+        self.comp = compName
         self.number = num
         self.robots = [] # [0:2] red, [3:5] blue
         n = 0
         for teamNumber in teamNumbers:
             # This will create a new robot each time it is called,
-            robots[n] = Robot(teamNumber,self, n, comp)
-            n += 1
+            robots[n] = Robot(teamNumber,self, n, self.comp)
+            n += 1  # [0:2] red, [3:5] blue
             pass
         
 '''
@@ -108,19 +130,27 @@ return self.index(tmpRoboList[0])
 # An instance of a robot in one match in one competician, will be diffent for the same teams robot in diffent matches and competicians
 class Robot:
     
-    def __init__(self, teamNumber, myMatch, num, comp):
+    def __init__(self, teamNumber, myMatch, num, compName):
+        self.comp = compName
+        self.match = myMatch
         self.teamNumber = teamNumber
-        self.num = allianceNumber
+        self.num = num
         if allianceNumber < 3:
             self.alliance = 'RED'
         else:
             self.alliance = 'BLUE'
 
-        records = RobotRecords(comp.name,myMatch,self.teamNumber,self.alliance)
+        records = RobotRecords(comp,self.match,self.teamNumber,self.alliance)
 
                         
 class RobotRecords:
-    def __init__(self, comp, myMatch, robot, alliance):
+    def __init__(self, compName, myMatch, robot, ally):
+        self.comp = compName
+        self.match = myMatch
+        self.roboNum = robot
+        self.alliance = ally # string (RED or BLUE)
+        
+        
         pass
         # variables being recorded ex)shots missed, points scored, climberlevel reached
                 
