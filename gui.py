e57a4f71c8d2c0515cf1ffd89be6165711ca86cb
Cougar Tech <cougarTech2228@users.noreply.github.com>
2014-01-14 21:01:16 -0500

    moved clock and font

--- gui.py
+++ gui.py
@@ -2,7 +2,10 @@
 
 class Gui
         def __init__(self):
+                
+         self.clock = pygame.time.Clock()
 
+         self.font = pygame.font.Font(None, 24)
 
         # Initiates the match window
         def draw(self): # this should eventually have options 
@@ -10,9 +13,6 @@ class Gui
 
             self.screen = pygame.display.set_mode((400, 300))
             pygame.display.set_caption('CATS: CougarTech Scouting Application')
-            self.clock = pygame.time.Clock() #does this belong here
-
-            self.font = pygame.font.Font(None, 24)
-        
+            
         def clear(self):
             self.screen.fill((0,0,0))
