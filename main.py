326067bf4137516e829462e3f22329d67cdd0d6e
User <team2228@ubuntu.ubuntu-domain>
2014-01-23 20:48:22 -0500

    worked on state

--- main.py
+++ main.py
@@ -9,39 +9,37 @@ import threading
 
 class Main():
     def __init__(self):
-        self.inputs = joy.joystick_init()
-        self.data = Data(len(inputs))
-        user.init()
         self.state = State()
+        self.inputs = joy.joystick_init()
+        self.data = Data(self, len(inputs))
+        user.init()
+        
        
-    def enterMatchMode(self):
-        self.state.inMatch = True
-        self.state.matchReadyStart = True
-        self.state.inMatch = True
-        self.state.inMatch = True
-        joy.pause = True
-        joy.end = False
-        t = threading.thread(target = joy.run)
-        t.start()
+
+        
         
 class Data(): 
-    def __init__(self, numInputs): #reminder -this must be fixed
+    def __init__(self,main, numInputs): #reminder -this must be fixed
         self.compList = data.CompetitianList()
         self.Robots = data.RobotList()
+        
+        self.state = main.state
         self.state.currentComp = self.compList[-1]
         self.state.currentMatch = self.compList[-1][-1]
+        
         self.temp_records  = []
         self.matchEvtList = None #evt list
+        
         for i in range(numInputs):
             self.temp_records.append(None) #can this be done
         pass
 
     def matchCreate(robots, placement=None):
         if placement is None:
-            self.currentComp.newMatch(robots)
+            self.state.currentComp.newMatch(robots)
             
         else:
-            self.currentComp.newMatch(robots, placement)
+            self.state.currentComp.newMatch(robots, placement)
             
     def setRobots(self, joy, robot): #set robots for match with inputs
         self.temp_records[joy] = data.InMatchRobotRecords(robot.myMatch.comp.name, robot.myMatch.matchNum, robot.alliance)
@@ -54,18 +52,19 @@ class Data():
     def commitMatch(self):
         for i in temp_records:
             i.tally
-            for r in self.currentRobots:
-                pass
-        if r.name == i.name:
+            for r in self.state.currentMatch.robots:
+                if r.name == i.name:
                     r.records = i
-        self.currentMatch.events = self.matchEvtList
+        self.state.currentMatch.events = self.matchEvtList
         
-
+    def setMatch(self, match, comp = None):
+        if comp != self.state.currentComp.name:
+            pass
+            
     
-    #this will now need to find the correct robot objects and append a inmatch robot records to its match list
 
 class State():
-    def __init__(self):
+    def __init__(self, ):
         self.reset()
         
     def getState(self):
@@ -82,12 +81,57 @@ class State():
         self.matchPaused = False
         self.matchStopped = False
         self.matchRunning = False
+        
         self.currentComp = None
         self.currentMatch = None
-
+        
+        self.matchIsSetup = False
+        
+        
+    def enterMatchMode(self):
+        self.inMatch = True
+        self.matchReadyStart = True
+        
+        joy.pause = True
+        joy.end = False
+        self.t = threading.thread(target = joy.run)
+        self.t.start()
+        
+    def togglePause(self):
+        if joy.pause == True:
+            joy.pause = False
+        else:
+            joy.pause = True
+            
+    def startMatch(self):
+        pass
+        
+    def endMatch(self):
+        pass
+        
+    def resetMatch(self):
+        pass
+        
+    def exitMatchMode(self):
+        self.inMatch = False
+        
+        self.matchReadyStart = False
+        self.matchReadyCommit = False
+        self.matchPaused = False
+        self.matchStopped = False
+        self.matchRunning = False
+        
+        joy.pause = False
+        joy.end = True
+        
     
-
-
+        
+        ''' get data from automatically gather able sources such as joy.pause and joy.end
+    def refresh(self):
+        self.matchPaused 
+        self.matchStopped
+        '''
+        
 if __name__ == "__main__": #This part is so that when it is imported, the following code doesn't run   
     main = Main()
     main.set_up()
